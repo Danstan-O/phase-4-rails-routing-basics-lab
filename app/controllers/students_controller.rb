6881b4ef977c2bf1eada2b6c00d9ef3ae491c61e
danstan <danstan.oduor@student.moringaschool.com>
2022-11-28 11:08:37 +0300

    adding routes

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,15 @@
+class StudentsController < ApplicationController
+    def index
+        students=Student.all
+        render json: students
+    end
+    def grades
+        grades=Student.all.order(grade: :desc)
+        render json: grades
+    end
+
+    def highest_grade
+        render json: Student.all.order(grade: :desc).limit(1)[0]
+    end
+
+end
